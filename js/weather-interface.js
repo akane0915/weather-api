112bb646fcfb7cf12d26ce27516bda21593958e1
Asia Kane and Tyler Stephenson <tyler@>
2017-05-23 09:40:26 -0700

    build out gulpfile and base js logic.

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,7 +1,10 @@
-$(function(){
-  $('#weather-location').click(function() {
-    var location = $('#location').val();
+var Weather = require('./../js/weather.js').weatherModule;
 
-  })
+$(document).ready(function() {
+  $('#weather-location').click(function() {
+    event.preventDefault();
+    var location = $('#location').val();
+    console.log(location);
+  });
 
 }); //document ready close
--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,8 @@
-function Weather () {
+function Weather (city, tmpF, tmpC, humidity) {
+  this.city = city;
+  this.tmpF = tmpF;
+  this.tmpC = tmpC;
+  this.humidity = humidity;
 }
 
-exports.weatherModule = Weather
+exports.weatherModule = Weather;
